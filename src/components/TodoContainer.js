6d044757b90d04ddd113cbfff0098a8ef3cf502f
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-01 20:33:42 +0430

    Initialize components

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import Header from './Header';
+import InputTodo from './InputTodo';
+import TodosList from './TodosList';
 
-export default function TodoContainer() {
-  return <div>TodoContainer</div>;
+export default class TodoContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Header />
+        <InputTodo />
+        <TodosList todos={this.state.todos} />
+      </>
+    );
+  }
 }
