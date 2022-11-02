0450bab3aa3d27b1751f23f2d1c827b4973f2b8f
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-02 15:02:10 +0430

    Add style file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoContainer from './components/TodoContainer';
+import './styles/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <TodoContainer />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
