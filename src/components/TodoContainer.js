01c789d36cf91bf2c0f0a7eab325c55e454aba4a
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-02 19:08:35 +0430

    Remove static data

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -8,23 +8,7 @@ export default class TodoContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [
-        {
-          id: 1,
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: 2,
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: 3,
-          title: 'Deploy to live server',
-          completed: false,
-        },
-      ],
+      todos: [],
     };
   }
 
