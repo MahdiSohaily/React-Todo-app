8afb6421dc5e40976eaa39fa4cda1abc7e62f626
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-02 14:29:58 +0430

    Add todo form submit method

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function InputTodo() {
+  const [title, setTitle] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(title);
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="Add Todo..." />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        placeholder="Add Todo..."
+      />
       <button type="submit">Submit</button>
     </form>
   );
