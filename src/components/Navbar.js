e5cbc361138a273c502934ff3ab8d9ec4a777465
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-03 16:32:49 +0430

    Add script to close menu on navigating between pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,11 @@ export default function Navbar() {
   const handleToggle = () => {
     setNavbarOpen(!navbarOpen);
   };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
   return (
     <>
       <nav className="navBar">
@@ -14,10 +19,14 @@ export default function Navbar() {
         </button>
         <ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink onClick={() => closeMenu()} to="/">
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/About">About</NavLink>
+            <NavLink onClick={() => closeMenu()} to="/About">
+              About
+            </NavLink>
           </li>
         </ul>
       </nav>
