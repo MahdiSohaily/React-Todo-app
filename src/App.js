b83d00500ecb24cff6a30049c8f091c1d4df80c0
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-17 15:37:43 +0430

    Removed project redux banner

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import Footer from './features/footer/Footer';
 function App() {
   return (
     <div className="App">
-      <nav>
-        <section>
-          <h1>Redux Fundamentals Example</h1>
-        </section>
-      </nav>
       <main>
         <section className="medium-container">
           <h2>Todos</h2>
