668b18c71b50f1dc2895d98211932064de69de66
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-20 17:29:42 +0430

    Add script to display active items count

--- src/features/footer/RemainingTodos.jsx
+++ src/features/footer/RemainingTodos.jsx
@@ -1,11 +1,21 @@
-/* eslint-disable react/prop-types */
-const RemainingTodos = ({ count }) => {
+import { useSelector } from 'react-redux';
+import { selectTodos } from '../../utilities/todo/selector';
+
+const RemainingTodos = () => {
+  const count = useSelector((state) => {
+    const todos = Object.values(selectTodos(state));
+    return todos.filter((item) => item.completed === false).length;
+  });
   const suffix = count === 1 ? '' : 's';
 
   return (
     <div className="todo-count">
       <h5>Remaining Todos</h5>
-      <strong>{count}</strong>
+      <strong>
+        {' '}
+        {count}
+        {' '}
+      </strong>
       item
       {suffix}
       {' '}
