ae5f887d555ae8ea1f069ad6fbb26da7a1c1078b
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-20 11:42:41 +0430

    Add filter reducer to root reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import todoReducer from './todos/todoSlice';
+import filterReducer from './filters/filterSlice';
 
 const rootReducer = combineReducers({
   todos: todoReducer,
+  filters: filterReducer,
 });
 
 export default rootReducer;
