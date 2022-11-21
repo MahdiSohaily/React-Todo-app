2353cd1341da35a7d8bf108fbb4e4d31f8cc0264
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-21 09:47:55 +0430

    Add custom middleware to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,22 @@
-import { applyMiddleware, createStore } from 'redux';
+/* eslint-disable no-unused-vars */
+import createStore, { applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducer';
+import client from '../api/client';
 
 const fetchTodosMiddleware = (storeApi) => (next) => (action) => {
   if (action.type === 'todos/fetchTodos') {
+    client.get('todos').then((todos) => {
+      console.log(todos);
+    });
   }
+  next(action);
 };
 
-const store = createStore(rootReducer, applyMiddleware(fetchTodosMiddleware));
+const customeEnhencer = composeWithDevTools(
+  applyMiddleware(fetchTodosMiddleware),
+);
+
+const store = createStore(rootReducer, customeEnhencer);
 
 export default store;
