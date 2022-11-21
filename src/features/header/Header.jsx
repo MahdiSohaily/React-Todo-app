d75b66984d58f734868287a391103b78932e11a2
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-21 11:43:12 +0430

    Update loading logic for input

--- src/features/header/Header.jsx
+++ src/features/header/Header.jsx
@@ -4,27 +4,33 @@ import { saveNewTodo } from '../../redux/todos/todoSlice';
 
 export default function Header() {
   const [text, setText] = useState('');
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
 
   const handleChanges = (e) => setText(e.target.value);
-  const handleKeyDown = (e) => {
+  const handleKeyDown = async (e) => {
     const trimmedText = text.trim();
     if (e.which === 13 && trimmedText) {
-      dispatch(saveNewTodo(trimmedText));
-
+      setLoading(true);
+      await dispatch(saveNewTodo(trimmedText));
+      setLoading(false);
       setText('');
     }
   };
+  const placeholder = loading ? 'loading...' : 'What needs to be done?';
+  const loader = loading ? <div className="loader" /> : null;
 
   return (
     <header className="header">
       <input
         className="new-todo"
-        placeholder="What needs to be done?"
+        placeholder={placeholder}
         value={text}
         onChange={handleChanges}
         onKeyDown={handleKeyDown}
+        disabled={loading}
       />
+      {loader}
     </header>
   );
 }
