8db5f8f4566f7895d0e955bd0e62159bcd903e28
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-21 09:43:17 +0430

    Add redux dev tools package

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './reducer';
 
-const store = createStore(rootReducer);
+const fetchTodosMiddleware = (storeApi) => (next) => (action) => {
+  if (action.type === 'todos/fetchTodos') {
+  }
+};
+
+const store = createStore(rootReducer, applyMiddleware(fetchTodosMiddleware));
 
 export default store;
